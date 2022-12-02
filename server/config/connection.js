55ec047c8e2405935fef9bdbec13a572e9582f2a
Richard Hong <rhong24@gmail.com>
2022-12-01 20:28:26 -0600

    adding apollo-server-express, graphql dependencies to server package.json/ adding bootstrapped server code to test functionality of graphql queries/ tested that npm run develop script works as expected

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-set-up',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 module.exports = mongoose.connection;
