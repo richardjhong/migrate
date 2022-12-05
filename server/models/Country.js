d7ac34bc7d5165b835681550244e3ef8c61f9061
rogseo <rogseo@gmail.com>
2022-12-05 10:19:29 -0600

    Add bhn, opp, fow data

--- server/models/Country.js
+++ server/models/Country.js
@@ -1,4 +1,32 @@
 const { Schema, model } = require('mongoose');
+// const Bhn = require('./Bhn');
+
+const bhnSchema = new Schema(
+    {
+        score_nbmc:Number,
+        score_ws: Number,
+        score_sh:Number,   
+        score_ps:Number,
+    }
+);
+
+const fowSchema = new Schema(
+    {
+        score_pr:Number,
+        score_pfc: Number,
+        score_incl:Number,   
+        score_aae:Number,
+    }
+);
+
+const oppSchema = new Schema(
+    {
+        score_abk:Number,
+        score_aic: Number,
+        score_hw:Number,   
+        score_eq:Number,
+    }
+);
 
 
 const countrySchema = new Schema(
@@ -7,16 +35,22 @@ const countrySchema = new Schema(
             type: String,
             required: true,
         },
+        //SPI Rank
         rank_score_spi:Number,
+        //Ranked or not
         status: String,
         score_spi:Number,
+        //Basic Human Needs
         score_bhn:Number,
+        bhn : bhnSchema,
+        //Foundations of Wellbeing
         score_fow:Number,
+        fow : fowSchema,
+        //Opportunity
         score_opp:Number,
-
+        opp : oppSchema,
+        
     }
-
-
 
 
 );
--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,7 @@
 // const User = require('./User');
 const Country = require('./Country');
+//Basic Human Need
+const Bhn = require('./Bhn');
 
 // module.exports = { User, Country };
-module.exports = { Country };
+module.exports = { Country, Bhn };
