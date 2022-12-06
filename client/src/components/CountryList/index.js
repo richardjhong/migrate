8956e2cb9a1a5803f12168e73ed6901fcc3925bc
Richard Hong <rhong24@gmail.com>
2022-12-05 18:10:28 -0600

    using spread operator on countries from destructured props to further destructure and exclude 0th index entry of props.countries which is effectively a header in csv content that was converted

--- client/src/components/CountryList/index.js
+++ client/src/components/CountryList/index.js
@@ -5,17 +5,21 @@ const CountryList = ({ countries, title }) => {
     return <h3>No Countries Retrieved From Database</h3>;
   }
 
+  // skip over 0th index entry of countries which is effectively a header in 
+  // in csv content
+  const [headers, ...countryListings] = [...countries]
+
   return (
     <div>
       <h3>{title}</h3>
-      {countries &&
-        countries.map((country) => (
+      {countryListings &&
+        countryListings.map((country) => (
           <div key={country._id} className="card mb-3">
             <h4 className="card-header bg-primary text-light p-2 m-0">
               {country.country} 
             </h4>
             <div className="card-body bg-light p-2">
-              <p>{country.rank_score_spi}</p>
+              <p>{country.country}'s rank_score_spi: {country.rank_score_spi}</p>
             </div>
           </div>
         ))}
