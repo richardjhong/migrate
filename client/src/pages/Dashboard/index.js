2c05e82d2403c249bc0b9a5417e0d2bf648816c7
Sarah Thoorens <[s.thoorens@gmail.com]>
2022-12-07 23:04:55 -0600

    local storage for search history

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../../utils/queries';
@@ -6,13 +6,33 @@ import Auth from '../../utils/auth';
 
 const Dashboard = () => {
   const { username: userParam } = useParams();
-
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam },
   });
-
   const user = data?.me || data?.user || {};
-  // navigate to personal profile page if username is yours
+  
+  // Store search history in local storage
+  const [showSearchHistory, setShowSearchHistory] = useState(true);
+
+  // Get search history from local storage and set to state
+  useEffect(() => {
+    const data = window.localStorage.getItem('Search History');
+    if (data !== null) {
+      setShowSearchHistory(JSON.parse(data));
+  }}, []);
+
+  // Set search history in local storage when state changes
+  useEffect(() => {
+    window.localStorage.setItem('Search History', JSON.stringify(showSearchHistory))
+  }, [showSearchHistory]);
+
+  let lastSearch = searchHistory[searchHistory.length - 1];
+	// render movie posters of stored movies to the DOM
+	if(searchHistory) { 
+		for (var i = 0; i < 3; i++) {
+		const searchHistoryContent = 
+    }}
+  // navigate to personal profile page if username matches param
   if (Auth.loggedIn() && Auth.getDashboard().data.username === userParam) {
     return <Navigate to="/me" />;
   }
@@ -34,9 +54,11 @@ const Dashboard = () => {
     <div>
       <div className="flex-row justify-center mb-3">
         <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
-          Viewing {userParam ? `${user.username}'s` : 'your'} recent searches.
+          Welcome {userParam ? `back, ${user.username}!` : 'Welcome!'} Continue where you left off..
         </h2>
-
+        <div>
+{     searchHistoryContent}
+  </div>
         <div>
          
         </div>
