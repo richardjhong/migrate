b2d637a5152428bce89ad12db0004b06623da82d
Sarah Thoorens <[s.thoorens@gmail.com]>
2022-12-08 14:10:43 -0600

    change user model

--- server/models/User.js
+++ server/models/User.js
@@ -19,7 +19,7 @@ const userSchema = new Schema(
       required: true,
       minlength: 5,
     },
-    savedCountries: 
+    searchedCountries: 
       {
         type: Schema.Types.ObjectId,
         ref: 'Country'
