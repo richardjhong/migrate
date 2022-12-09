b0ba055727127bcbc2d3619902dae3b46f3fa81a
Richard Hong <rhong24@gmail.com>
2022-12-08 20:42:14 -0600

    changing nested information within opp and fow within QUERY_COUNTRIES on client side as well

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -38,17 +38,17 @@ query allCountries {
     }
     score_fow
     fow {
-      score_aae
-      score_incl
-      score_pfc
-      score_pr
-    }
-    score_opp
-    opp {
       score_abk
       score_aic
       score_eq
       score_hw
+    }
+    score_opp
+    opp {
+      score_aae
+      score_incl
+      score_pfc
+      score_pr
     }
   }
 }
