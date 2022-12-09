9bfceb30a9fe2488e75b3dffc53158b7fc127727
Sarah Thoorens <[s.thoorens@gmail.com]>
2022-12-09 14:35:16 -0600

    dashboard search history rendering is functional.

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -2,16 +2,29 @@ import React from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../../utils/queries';
-import SearchHistoryList from '../../components/SearchHistory';
+import SearchCountry from '../../components/SearchCountry';
+import { useSearch } from '../../utils/CountryContext';
+import { QUERY_SINGLE_COMPILATION} from '../../utils/queries';
 import Auth from '../../utils/auth';
 import "./dashboard.scss";
+import { saveCountries } from '../../utils/localStorage';
 
 const Dashboard = () => {
+
+  
   const { username: userParam } = useParams();
-  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-    variables: { username: userParam },
+  const { loading: loadingC, data: dataC  } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+    variables: { username: userParam}
   });
-  const user = data?.me || data?.user || {};
+  const user = dataC?.me || dataC?.user || {};
+
+  const { searches } = useSearch();
+  console.log(searches);
+
+  const { loading, data } = useQuery(QUERY_SINGLE_COMPILATION,{
+    variables:{countryname : searches[0]}
+  });
+  console.log(loading, data)
 
   const handleLogout = async (e) => {  
     e.preventDefault();
@@ -26,7 +39,7 @@ const Dashboard = () => {
     return <Navigate to="/me" />;
   }
 
-  if (loading) {
+  if (loadingC) {
     return <div>Loading...</div>;
   }
 
@@ -42,24 +55,32 @@ const Dashboard = () => {
     );
     }
   return (
-    <div>
+    <div key={user.saveCountries}>
       <div className="flex-row justify-center mb-3">
         <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
           Welcome {userParam ? `back, ${user.username} continue where you left off.. !` : `back, ${user.username}!`} 
         </h2>
-          <div>
-            <h3>Your Most Recent Search: </h3>
+          <div >
+            <h3 key={user}>Your Most Recent Searches: </h3>
             <div className='countryCard' id="col2row1">
-          <SearchHistoryList
-            thoughts={user.searchHistory}
-            title={`${user.username}'s search history...`}
-            showTitle={false}
-            showUsername={false}
-          />
+            {loading ? (
+                <div>Loading...</div>
+              ) : 
+             searches && searches.map((search) => (
+            <div>
+              <h4 className="card-header bg-primary text-light p-2 m-0">
+                <a href='/singleCountry/${search}'>{search}</a>
+              </h4>
             </div>
+          )) } 
+          </div>
+          <div>
+            <h3>Start a new search here: <SearchCountry className="button"/></h3>
           </div>
         </div>
+       
         <button type="submit" onClick={handleLogout}>Log Out</button>
+    </div>
     </div>
   );
 };
