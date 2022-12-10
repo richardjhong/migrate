82d6daca908ec74eda90ccb4c482d02934a0d00d
Mark Gardner <themarkgardner@gmail.com>
2022-12-09 21:05:51 -0600

    adjusted styling for dashboard pages

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -42,10 +42,12 @@ const Dashboard = () => {
     );
     }
   return (
-    <div>
-      <div className="flex-row justify-center mb-3">
-        <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
-          Welcome {userParam ? `back, ${user.username} continue where you left off.. !` : `back, ${user.username}!`} 
+    <>
+        <Header />
+    <main class='dashMain'>
+      <div className="">
+        <h2 className="">
+          Welcome {userParam ? `back, ${user.username} continue where you left off.. !` : `back, ${user.username}!`}
         </h2>
           <div>
             <h3>Your Most Recent Search: </h3>
