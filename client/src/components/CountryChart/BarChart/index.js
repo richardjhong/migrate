dd2cc0ce99973f951b6d82fb5ffc3a17a2d4c3bf
Richard Hong <rhong24@gmail.com>
2022-12-11 14:12:59 -0600

    removing commented out code in BarChart, removing position:absolute within chartContainer.scss

--- client/src/components/CountryChart/BarChart/index.js
+++ client/src/components/CountryChart/BarChart/index.js
@@ -24,12 +24,10 @@ const BarChart = ({
               textAnchor={({ text }) => text.length > 1 ? "start" : "middle"}
             />
           }
-          // alignment="start"
           style={{
             data: { 
               fill: "#04566e",
               stroke: "#b4d330",
-              // fillOpacity: 0.7,
               strokeWidth: 3
             },
             parent: { border: "1px solid #ccc"},
@@ -46,15 +44,6 @@ const BarChart = ({
             { x: "2022", y: fields["2022"] },
           ]}
         />
-        {/* <VictoryScatter
-          data={[
-            { x: "2018", y: fields["2018"]},
-            { x: "2019", y: fields["2019"] },
-            { x: "2020", y: fields["2020"] },
-            { x: "2021", y: fields["2021"] },
-            { x: "2022", y: fields["2022"] }
-          ]}
-        /> */}
         <VictoryAxis crossAxis
           label="Year"
         />
