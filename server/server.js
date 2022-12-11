9beedd6f7d1bd272fc39e9c94f3ad9858d428407
Richard Hong <rhong24@gmail.com>
2022-12-11 16:16:26 -0600

    adding more config changes to server/server.js for heroku deployment with apollo

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
+const PORT = Number.parseInt(process.env.PORT) || 3001;
 const app = express();
 
 const server = new ApolloServer({
@@ -15,6 +15,8 @@ const server = new ApolloServer({
   context: authMiddleware,
   introspection: true
 });
+
+const { url } = await startStandaloneServer(server, { listen: { port } });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
