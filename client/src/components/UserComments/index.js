3ddc56d522df1706fe0ccb7016be90b518cbf0ef
Mark Gardner <themarkgardner@gmail.com>
2022-12-12 12:35:03 -0600

    integrated Yeon's code. Mapping values seem odd but function seems stable

--- client/src/components/UserComments/index.js
+++ client/src/components/UserComments/index.js
@@ -1,8 +1,7 @@
-
 import React, { useState, useEffect } from 'react';
 import "./UserComments.scss"
 
-import SearchCountry from '../../components/SearchCountry';
+import SearchCountry from '../SearchCountry';
 import { useSearch } from '../../utils/CountryContext';
 import { useNavigate } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
@@ -10,19 +9,6 @@ import { useQuery } from '@apollo/client';
 import { QUERY_COUNTRY_COMMENTS} from '../../utils/queries'
 
 
-// const Comments = () => {
-//     const { countryname } = useParams();
-//     const { loading, data } = useQuery(QUERY_COUNTRY_COMMENTS, {
-//     variables: {country: countryname},
-//     }
-//     );
-// }
-
-// const comments=[{commentText: 'I really love the festivals in Galway', commentAuthor:'Mark'}, {commentText:'You can get by with English in Paris, but you might struggle in the countryside.', commentAuthor:'Buffy'}, {commentText:"Best Indian food in the UK is Dishoom, so move somewhere close to there.", commentAuthor:"River"}]
-// const ticker = comments.map(({commentText, commentAuthor}) => 
-//     (<div key={commentText +'1'}><div className='tickerText' key={commentText +'2'}>{commentText} - <span className='tickerAuthor' key={commentText +'3'}>{commentAuthor}</span></div></div>)
-// )
-// console.log(comments)
 
 export default function UserComments() {
 
@@ -31,41 +17,57 @@ export default function UserComments() {
         variables: {country:countryname},
         }
         );
- 
-//testing
-    console.log(data)
+
+
+   if(data){
+    console.log(data);
     
-    const ticker = !loading && data.map(({commentText, commentAuthor}) => 
-    (<div key={commentText +'1'}><div className='tickerText' key={commentText +'2'}>{commentText} - <span className='tickerAuthor' key={commentText +'3'}>{commentAuthor}</span></div></div>)
- )
- console.log('ticker')
- 
-
-    // const { searches } = useSearch();
-    // const { country: countryParam } = useParams();
-    // const navigate = useNavigate();
-
-    // let index = '';
-    // if (countryParam) {
-    //     index = searches.findIndex(country => country.name === countryParam);
-    // }
+   }
 
     return (
 
 
         <div className='tickerWrapper'>
             <div className='tickerContent'>
-            {/* {loading || ticker===undefined ? (
-            <div>Loading...</div>
-          ) : (
-            {ticker}
-              
-            
-          )} */}
-                
-            </div>
-        </div>
-    )
+            {(loading || data.commentCountry === null )? (
+                <div>Loading...</div>
+        ) : (
+            <>
+            {data.commentCountry.map((comment, i) => {
+              return (
+                <div
+                  key={i}
+                  className='tickerText'
+                >
+                  <div
+                  className='tickerText' key={i}>
+                    {comment._id} - 
+                    <span
+                    className='tickerAuthor' key={i}
+                    >
+                      {comment.commentAuthor}
+                    </span>
+                  </div>
+                </div>
+              );
+            })}
+
+
+          </>
+        )}
+
+      </div>
+    </div>
+
+  )
 
 }
+ 
+//testing
+//     console.log(data)
+    
+//    
+//  console.log('ticker')
+ 
+
 
