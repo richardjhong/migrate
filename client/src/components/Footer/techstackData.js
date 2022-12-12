b08a043a3cc8bbdee6c73e3955c75e027d3872ca
Richard Hong <rhong24@gmail.com>
2022-12-11 21:34:31 -0600

    adding react-tooltip and react-icons, making a techstackData import within Footer, adding techStack with hoverable tooltips to Footer, including some styling in Footer.scss

---
+++ client/src/components/Footer/techstackData.js
@@ -0,0 +1,47 @@
+import { SiJavascript, SiReact, SiExpress, SiHtml5, SiCss3, SiHeroku, SiMongodb, SiGreensock } from "react-icons/si";
+import { FaNode, FaSass } from "react-icons/fa";
+
+export const websiteTechStack = {
+  devicons: [
+    {
+      icon: SiJavascript,
+      tooltipMessage: "JavaScript"
+    },
+    {
+      icon: SiReact,
+      tooltipMessage: "React"
+    },
+    {
+      icon: SiHtml5,
+      tooltipMessage: "HTML5"
+    },
+    {
+      icon: SiCss3,
+      tooltipMessage: "CSS3"
+    },
+    {
+      icon: SiGreensock,
+      tooltipMessage: "GreenSock"
+    },
+    {
+      icon: SiMongodb,
+      tooltipMessage: "MongoDB"
+    },
+    {
+      icon: FaSass,
+      tooltipMessage: "Sass"
+    },
+    {
+      icon: FaNode,
+      tooltipMessage: "Node"
+    },
+    {
+      icon: SiExpress,
+      tooltipMessage: "Express"
+    },
+    {
+      icon: SiHeroku,
+      tooltipMessage: "Heroku"
+    }
+  ]
+}
