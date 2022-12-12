5728ba1e0badae84dd4b3e1652abcc9acce2697f
Mark Gardner <themarkgardner@gmail.com>
2022-12-12 15:47:28 -0600

    added lots of clean up css for alignments and colors. Imported new icons for single country page.

--- client/src/components/UserComments/index.js
+++ client/src/components/UserComments/index.js
@@ -11,49 +11,49 @@ import { QUERY_COUNTRY_COMMENTS } from '../../utils/queries'
 
 export default function UserComments() {
 
-  const { countryname } = useParams();
-  const { loading, data } = useQuery(QUERY_COUNTRY_COMMENTS, {
-    variables: { country: countryname },
-  }
-  );
+    const { countryname } = useParams();
+    const { loading, data } = useQuery(QUERY_COUNTRY_COMMENTS, {
+        variables: { country: countryname },
+    }
+    );
 
 
-  if (data) {
-    console.log(data);
+    if (data) {
+        console.log(data);
 
-  }
+    }
 
-  return (
+    return (
 
-    <div className='tickerWrapper'>
-      <div className='tickerContent'>
-        {(loading || data.commentCountry === null) ? (
-          <div>Loading...</div>
-        ) : (
-          <>
-            {data.commentCountry.map((comment, i) => {
-              return (
-                <div
-                  key={i}
-                  className='tickerText'
-                >
-                  <div
-                  className='tickerText' key={i}>
-                    {comment.commentText} - 
-                    <span
-                    className='tickerAuthor' key={i}
-                    >
-                      {comment.commentAuthor}
-                    </span>
-                  </div>
-                </div>
-              );
-            })}
+        <div className='tickerWrapper'>
+            <div className='tickerContent'>
+                {(loading || data.commentCountry === null) ? (
+                    <div>Loading...</div>
+                ) : (
+                    <>
+                        {data.commentCountry.map((comment, i) => {
+                            return (
+                                <div
+                                    key={i}
+                                    className='tickerTextContain'
+                                >
+                                    <div
+                                        className='tickerText' key={i}>
+                                        {comment._id} -
+                                        <span
+                                            className='tickerAuthor' key={i}
+                                        >
+                                            {comment.commentAuthor}
+                                        </span>
+                                    </div>
+                                </div>
+                            );
+                        })}
 
-          </>
-        )}
+                    </>
+                )}
 
-      </div>
-    </div>
-  )
+            </div>
+        </div>
+    )
 }
