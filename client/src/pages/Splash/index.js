9ec98ad70e3bd7838988920bb0953dc6bc41aca4
Mark Gardner <themarkgardner@gmail.com>
2022-12-12 20:42:39 -0600

    added media queries to splash screen for smaller sizes

--- client/src/pages/Splash/index.js
+++ client/src/pages/Splash/index.js
@@ -12,9 +12,11 @@ function Splash() {
             <main className="splash">
                 <p className='tag'>Looking to relocate? Start your search here.</p>
                 <h1 className='migrate'>MIGRATE</h1>
+                <div className="searchRespCenter">
                 <SearchCountry
                 
                 />
+                </div>
                 {/* <button class="button">Where are you going?...</button> */}
                 <p className="overview"> Get the hard data about your dream country.
                     Migrate compiles years of information to present a general overview of any country. There are so many factors that
