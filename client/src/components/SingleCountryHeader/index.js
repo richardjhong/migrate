32b4d1b352a975ca883612797e1d61b0d6220fa1
Richard Hong <rhong24@gmail.com>
2023-03-25 12:17:34 -0500

    merging with latest changes to main

--- client/src/components/SingleCountryHeader/index.js
+++ client/src/components/SingleCountryHeader/index.js
@@ -5,8 +5,8 @@ import { useParams } from 'react-router-dom';
 
 const SingleCountryHeader = () => {
   const { countryname: countryParam } = useParams();
-
   const [imgs, setImgs] = useState([]);
+  
   useEffect(() => {
      fetch(`https://api.unsplash.com/search/photos?page=1&orientation=squarish&query=${countryParam}&client_id=${process.env.REACT_APP_ACCESS_KEY}`)
         .then((response) => response.json())
@@ -26,8 +26,6 @@ const SingleCountryHeader = () => {
            console.log(err.message);
         });
   }, [countryParam]);
-
-  
 
   return (
     <>
